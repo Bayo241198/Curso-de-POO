c91494c9f7923e126fd90ba9831c81d2b2765dc6
Bayo241198 <bryan.antezana.riveros@gmail.com>
2022-07-13 15:43:39 -0400

    Manejo de objetos en Java y Python

--- Java/Car.java
+++ Java/Car.java
@@ -1,6 +1,10 @@
-public class Car {
+class Car {
     Integer id;
     String license;
     String driver;
     Integer passenger;
+
+    void printDataCar(){
+        System.out.println("License: " + license + "Driver: " + driver + "Passenger: " + passenger);
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,15 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "ABC123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "XYZ456";
+        car2.driver = "Andrea herrera";
+        car.passenger = 3;
+        car.printDataCar();
     }
 }
