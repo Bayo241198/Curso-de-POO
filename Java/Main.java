baaec67a063338d53d676fdd6e90a73f37fb5482
Bayo241198 <bryan.antezana.riveros@gmail.com>
2022-07-14 15:08:21 -0400

    Constructores en Java agregados y nuevos archivos en JavaScript

--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,11 @@
 class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.license = "ABC123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("ABC123", new Account("Andres Herrera", "7296889"));
         car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "XYZ456";
-        car2.driver = "Andrea herrera";
-        car.passenger = 3;
-        car.printDataCar();
+        Car car2 = new Car("XYZ456", new Account("Andrea Herrera", "3553412"));
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
